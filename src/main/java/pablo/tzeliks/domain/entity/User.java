00ac3fea37a6723afb300ca4069123444f46f41b
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-06 14:48:37 -0300

    feat: add User entity

---
+++ src/main/java/pablo/tzeliks/domain/entity/User.java
@@ -0,0 +1,46 @@
+package pablo.tzeliks.domain.entity;
+
+import pablo.tzeliks.domain.entity.vo.Password;
+
+public class User {
+
+    private final Long id;
+    private String email;
+    private String Telephone;
+    private Password password;
+
+    public User(Long id, String email, String telephone, Password password) {
+        this.id = id;
+        this.email = email;
+        Telephone = telephone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return Telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        Telephone = telephone;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/pablo/tzeliks/domain/entity/vo/Password.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.domain.entity.vo;
+
+public class Password {
+}
